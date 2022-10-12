88fb2dc8bd9c5e4785c6da1ef570645516c8d0ca
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2022-10-12 09:35:12 +0200

    Try using an allocator

--- cat.zig
+++ cat.zig
@@ -1,34 +1,30 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 
 pub fn main() !void {
     const stdin = std.io.getStdIn().reader();
     const stdout = std.io.getStdOut().writer();
     
     // Read and print lines from standard input
-    var buffer: [std.mem.page_size]u8 = undefined;
+    var buffer: [100]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    const allocator = fba.allocator();
     while (true) {
-        const inputLine = try readLine(stdin, &buffer);
-        if (inputLine == null) break;
-        try stdout.print("{s}\n", .{inputLine});
-    }
-}
-
-/// readLine returns the next line, or null if we encounter a known error. We panic if we encounter an unknown error
-fn readLine(reader: std.fs.File.Reader, buffer: []u8) !?[]const u8 {
-    if (reader.readUntilDelimiter(buffer, '\n')) |line| {
-        return line;
-    } else |err| switch (err) {
-        // EOF
-        error.EndOfStream => {
-            return null;
-        },
-        // Line is longer than the buffer
-        error.StreamTooLong => {
-            try std.io.getStdErr().writer().print("Line was too long: Can only handle lines up to length {d}.\n", .{buffer.len});
-            return null;
-        },
-        else => {
-            std.debug.panic("Got unhandled error {s}\n", .{err});
-        },
+        if (stdin.readUntilDelimiterAlloc(allocator, '\n', std.mem.page_size)) |line| {
+            try stdout.print("{s}\n", .{line});
+        } else |err| switch (err) {
+            // EOF
+            error.EndOfStream => {
+                break;
+            },
+            // Line is longer than the buffer
+            error.StreamTooLong, error.OutOfMemory => {
+                try std.io.getStdErr().writer().print("Line was too long: Can only handle lines up to length {d}.\n", .{buffer.len});
+                std.os.exit(1);
+            },
+            else => {
+                std.debug.panic("Got unhandled error {s}\n", .{err});
+            },
+        }
     }
 }
